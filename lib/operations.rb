0497cdc5a73455a89a98814d33fcdadda9962355
Richard Pascarelli <rpavesi84@gmail.com>
2019-09-10 04:29:51 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,14 @@
 def unsafe?(speed)
-
+  if speed >= 61 || speed <=39 
+    p true
+  else
+    p false
+  end
 end
 
 
-
 def not_safe?(speed)
-	
+  speed >= 61 || speed <=39 ? true : false
 end
 	
 
