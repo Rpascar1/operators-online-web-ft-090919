127c4ee939a8ec05e96f13349ed26ba762fdf631
Richard Pascarelli <rpavesi84@gmail.com>
2019-09-10 04:29:04 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -9,12 +9,7 @@ end
 
 
 def not_safe?(speed)
-if speed >= 61 || speed <=39 ? true : false
-    p true
-  elsif
-    speed >=40 || speed <= 60
-    p false
-  end
+  speed >= 61 || speed <=39 ? true : false
 end
 	
 
